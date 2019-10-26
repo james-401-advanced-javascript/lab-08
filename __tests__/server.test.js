861f1666ce35a9a537877eec8653e8ca3850fc24
James Dunn <jamesdu@zillowgroup.com>
2019-10-25 21:19:13 -0700

    Got people and teams set up along with testing

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -9,35 +9,113 @@ const mockServer = supertester(server);
 // (akin to server.start, we're doing server.server)
 
 describe('web server', () => {
-  test('should respond properly on a get request to /people', async () => {
+  it('should respond properly on a get request to /people', async () => {
     let results = await mockServer.get('/people');
-    console.log('RES: ', results.body);
     expect(results.status).toBe(200);
-    expect(results.body.count).toBe(4);
+  });
+  it('should respond properly on a post request to /people', async () => {
+    let person = {
+      firstName: 'Sarah',
+      lastName: 'Smalls',
+      birthday: '2020-05-10T07:00:00.000Z',
+      likes: 'dogs',
+      _team: '5da66623c1b7ab45a30e85b7',
+    };
+    try {
+      let data = await mockServer.post('/people').send(person);
+      let dbResults = await mockServer.get('/people/Sarah-Smalls');
+      expect(data.status).toBe(200);
+      expect(dbResults).toBeTruthy();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  it('should respond properly on a put request to /people', async () => {
+    let person = {
+      firstName: 'Sarah',
+      lastName: 'Smalls',
+      birthday: '2020-05-10T07:00:00.000Z',
+      likes: 'dogs',
+      _team: '5da66623c1b7ab45a30e85b7',
+    };
+    try {
+      let data = await mockServer.post('/people').send(person);
+      let id = data.body._id;
+      let updated = await mockServer
+        .put('/people/Sarah-Smalls')
+        .send(id, { firstName: 'Suzie' });
+      let dbResults = await mockServer.get('/people/Suzie-Smalls');
+      expect(data.status).toBe(200);
+      expect(dbResults).toBeTruthy();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  it('should respond properly on a delete request to /people', async () => {
+    let person = {
+      firstName: 'Sarah',
+      lastName: 'Smalls',
+      birthday: '2020-05-10T07:00:00.000Z',
+      likes: 'dogs',
+      _team: '5da66623c1b7ab45a30e85b7',
+    };
+    try {
+      let data = await mockServer.post('/people').send(person);
+      let id = data.body._id;
+      let updated = await mockServer.delete('/people/Sarah-Smalls').send(id);
+      expect(updated.status).toBe(200);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  it('should respond properly on a get request to /teams', async () => {
+    let results = await mockServer.get('/teams');
+    expect(results.status).toBe(200);
+  });
+  it('should respond properly on a post request to /teams', async () => {
+    let team = {
+      name: 'Black Panthers',
+      color: 'black',
+    };
+    try {
+      let data = await mockServer.post('/teams').send(team);
+      let dbResults = await mockServer.get('/teams/Black-Panthers');
+      expect(data.status).toBe(200);
+      expect(dbResults).toBeTruthy();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  it('should respond properly on a put request to /teams', async () => {
+    let team = {
+      name: 'Black Panthers',
+      color: 'black',
+    };
+    try {
+      let data = await mockServer.post('/teams').send(team);
+      let id = data.body._id;
+      let updated = await mockServer
+        .put('/teams/Black-Panthers')
+        .send(id, { name: 'Black Pumas' });
+      let dbResults = await mockServer.get('/people/Suzie-Smalls');
+      expect(data.status).toBe(200);
+      expect(dbResults).toBeTruthy();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  it('should respond properly on a delete request to /teams', async () => {
+    let team = {
+      name: 'Black Panthers',
+      color: 'black',
+    };
+    try {
+      let data = await mockServer.post('/teams').send(team);
+      let id = data.body._id;
+      let updated = await mockServer.delete('/teams/Black-Panthers').send(id);
+      expect(updated.status).toBe(200);
+    } catch (error) {
+      console.error(error);
+    }
   });
 });
-// it('should respond properly on a post request to /people', () => {
-//   let person = {
-//     firstName: 'Sarah',
-//     lastName: 'Smalls',
-//     birthday: '2020-05-10T07:00:00.000Z',
-//     likes: 'dogs',
-//     _team: '5da66623c1b7ab45a30e85b7',
-//   };
-//   mockServer
-//     .post('/people')
-//     .send(person)
-//     .then(data => {
-//       console.log('DATA: ', data);
-//     })
-//     .catch(error => console.error(error));
-//   try {
-//     let data = await mockServer.post('/people').send(person);
-//     let dbResults = await mockServer.get('/people');
-//     expect(data.status).toBe(200);
-//     expect(dbResults[0].firstName).toBe('Sarah');
-//   } catch (error) {
-//     console.error(error);
-//   }
-//   });
-// });
